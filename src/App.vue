20e6fd37a44c9bb48bdd7f633c89151a8c8b45ea
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-05-05 12:51:37 +0100

    advice app

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="contained">
+      <div v-if="result">
+        <h1 class="h1">Advice # {{ result.id }}</h1>
+        <p class="p">
+          {{ result.advice }}
+        </p>
+      </div>
+      <div v-else class="grid place-items-center">
+        <div class="lds-ellipsis">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div class="img">
+        <img src="./assets/images/pattern-divider-desktop.svg" alt="" />
+      </div>
+      <div class="mobile">
+        <img src="./assets/images/pattern-divider-mobile.svg" alt="" />
+      </div>
+      <button @click="getAdvice">
+        <div>
+          <img src="./assets/images/icon-dice.svg" alt="" />
+        </div>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+import axios from "axios";
+onMounted(async () => {
+  const { data } = await axios(url);
+  result.value = data.slip;
+});
+const url = "https://api.adviceslip.com/advice";
+const result = ref("");
+const getAdvice = async () => {
+  const { data } = await axios(url);
+  result.value = data.slip;
+};
+</script>
+
+<style>
+@import url("./assets/base.css");
+</style>
